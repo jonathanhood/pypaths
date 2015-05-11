3166f5359a142a012ac2922f6df3b282ae7e59cf
Jonathan Hood <me@jonathanhood.org>
2015-05-11 10:39:29 -0500

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pypaths',
-    version='0.1.1',
+    version='0.1.2',
     author='Jonathan Hood',
     author_email='me@jonathanhood.org',
     packages=['pypaths'],
